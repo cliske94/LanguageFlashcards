a3e7301b39432f098268a1b6c7b06e47411e893b
Cody Liske <34665198+cliske94@users.noreply.github.com>
2021-12-02 18:56:16 -0500

    Update TranslationDictionary.java

--- ProgrammingAssigment4/src/TranslationDictionary.java
+++ ProgrammingAssigment4/src/TranslationDictionary.java
@@ -1,18 +1,11 @@
+/**
+ * @author Cody Liske
+ */
 
 import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Enumeration;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author clisk
- */
 public class TranslationDictionary extends Dictionary {
     
     private ArrayList<String> wordBank1 = new ArrayList<String>();
